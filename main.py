f24868db219acbd20e8d87dbc0e51160283d99c8
wanggor <anggorow9@gmail.com>
2020-12-28 01:49:41 +0700

    change to new format data

--- main.py
+++ main.py
@@ -19,10 +19,6 @@ class Fuzzy(QMainWindow):
         super(Fuzzy, self).__init__()
         self.ui = uic.loadUi('gui.ui', self)
 
-        self.velocity_membership = {
-            "very_low" : ""
-        }
-
         self.vel_max = 85.5
         self.dis_max = 90.5
         self.brake_max = 360.5
@@ -45,13 +41,182 @@ class Fuzzy(QMainWindow):
         self.brake_high = [240, 300, 360]
         self.brake_very_high = [320, 330, 360, 360]
 
-        self.ui.deploy_btn.toggled.connect(self.deploy)
-        self.ui.deploy_btn.setCheckable(True)
+        self.ui.deploy_btn.clicked.connect(self.deploy)
+        self.ui.calc_btn.clicked.connect(self.calculate)
 
-        self.ui.calc_btn.toggled.connect(self.calculate)
-        self.ui.calc_btn.setCheckable(True)
-    
+        self.setup()
+
+    def setup(self):
+        self.velocity_membership = {
+            "very_low" : [0, 0, 10, 15],
+            "low" : [10, 22.5, 35],
+            "medium" : [30, 47.5, 65],
+            "high" : [55, 67.5, 80],
+            "very_high" : [70, 75, 85, 85]
+        }
+
+        self.distance_membership = {
+            "very_low" :[0, 0, 15, 30],
+            "low" : [15, 30, 45],
+            "medium" : [35, 55, 75],
+            "high" : [60, 75, 90],
+            "very_high" : [80, 85, 90, 90]
+        }
+
+        self.brake_membership = {
+            "very_low" :[0, 0, 40, 50],
+            "low" : [40, 100, 160],
+            "medium" : [120, 190, 280],
+            "high" : [240, 300, 360],
+            "very_high" : [320, 330, 360, 360]
+        }
+
+        self.velocity_input = {
+            "very_low" : [self.ui.line_vel_verlow_min, self.ui.line_vel_verlow_max],
+            "low" : [self.ui.line_vel_low_min, self.ui.line_vel_low_max],
+            "medium" : [self.ui.line_vel_med_min, self.ui.line_vel_med_max],
+            "high" : [self.ui.line_vel_high_min, self.ui.line_vel_high_max],
+            "very_high" : [self.ui.line_vel_verhigh_min, self.ui.line_vel_verhigh_max],
+        }
+
+        self.distance_input = {
+            "very_low" : [self.ui.line_dis_verlow_min, self.ui.line_dis_verlow_max],
+            "low" : [self.ui.line_dis_low_min, self.ui.line_dis_low_max],
+            "medium" : [self.ui.line_dis_med_min, self.ui.line_dis_med_max],
+            "high" : [self.ui.line_dis_high_min, self.ui.line_dis_high_max],
+            "very_high" : [self.ui.line_dis_verhigh_min, self.ui.line_dis_verhigh_max],
+        }
+
+        self.brake_input = {
+            "very_low" : [self.ui.line_brake_verlow_min, self.ui.line_brake_verlow_max],
+            "low" : [self.ui.line_brake_low_min, self.ui.line_brake_low_max],
+            "medium" : [self.ui.line_brake_med_min, self.ui.line_brake_med_max],
+            "high" : [self.ui.line_brake_high_min, self.ui.line_brake_high_max],
+            "very_high" : [self.ui.line_brake_verhigh_min, self.ui.line_brake_verhigh_max],
+        }
+
+        self.velocity_label = {
+            "very_low" : [self.ui.label_vel_1, self.ui.label_vel_2, self.ui.label_vel_3],
+            "low"  :[self.ui.label_vel_4, self.ui.label_vel_5, self.ui.label_vel_6],
+            "medium" : [self.ui.label_vel_7, self.ui.label_vel_8, self.ui.label_vel_9],
+            "high" :[self.ui.label_vel_10, self.ui.label_vel_11, self.ui.label_vel_12],
+            "very_high" :[self.ui.label_vel_13, self.ui.label_vel_14, self.ui.label_vel_15],
+        }
+
+        self.distance_label = {
+            "very_low" : [self.ui.label_dis_1, self.ui.label_dis_2, self.ui.label_dis_3],
+            "low"  :[self.ui.label_dis_4, self.ui.label_dis_5, self.ui.label_dis_6],
+            "medium" : [self.ui.label_dis_7, self.ui.label_dis_8, self.ui.label_dis_9],
+            "high" :[self.ui.label_dis_10, self.ui.label_dis_11, self.ui.label_dis_12],
+            "very_high" :[self.ui.label_dis_13, self.ui.label_dis_14, self.ui.label_dis_15],
+        }
+
+        self.brake_label = {
+            "very_low" : [self.ui.label_brake_1, self.ui.label_brake_2, self.ui.label_brake_3],
+            "low"  :[self.ui.label_brake_4, self.ui.label_brake_5, self.ui.label_brake_6],
+            "medium" : [self.ui.label_brake_7, self.ui.label_brake_8, self.ui.label_brake_9],
+            "high" :[self.ui.label_brake_10, self.ui.label_brake_11, self.ui.label_brake_12],
+            "very_high" :[self.ui.label_brake_13, self.ui.label_brake_14, self.ui.label_brake_15],
+        }
+
+        self.input_min_lis = [self.ui.line_vel_min, self.ui.line_dis_min, self.ui.line_brake_min]
+        self.input_max_lis = [self.ui.line_vel_max, self.ui.line_dis_max, self.ui.line_brake_max]
+
+        self.fill_input()
+        self.fill_label()
+
+    def fill_label(self):
+        membership_range = [self.velocity_membership, self.distance_membership, self.brake_membership]
+        label_list = [self.velocity_label, self.distance_label, self.brake_label]
+
+        for index in range(3):
+            self.input_min_lis[index].setText(str(membership_range[index]["very_low"][0]))
+            self.input_max_lis[index].setText(str(membership_range[index]["very_high"][3]))
+            for level in membership_range[index]:
+                if level == "very_low":
+                    label_list[index][level][0].setText(str(membership_range[index][level][1]))
+                    label_list[index][level][1].setText(str(membership_range[index][level][2]))
+                    label_list[index][level][2].setText(str(membership_range[index][level][3]))
+                elif level == "very_high":
+                    label_list[index][level][0].setText(str(membership_range[index][level][0]))
+                    label_list[index][level][1].setText(str(membership_range[index][level][1]))
+                    label_list[index][level][2].setText(str(membership_range[index][level][2]))
+                else:
+                    label_list[index][level][0].setText(str(membership_range[index][level][0]))
+                    label_list[index][level][1].setText(str(membership_range[index][level][1]))
+                    label_list[index][level][2].setText(str(membership_range[index][level][2]))
+
+
+
+    def fill_input(self):
+
+        membership_range = [self.velocity_membership, self.distance_membership, self.brake_membership]
+        input_list = [self.velocity_input, self.distance_input, self.brake_input]
+        
+        for index in range(3):
+            self.input_min_lis[index].setText(str(membership_range[index]["very_low"][0]))
+            self.input_max_lis[index].setText(str(membership_range[index]["very_high"][3]))
+            for level in membership_range[index]:
+                if level == "very_low":
+                    input_list[index][level][0].setText(str(membership_range[index][level][2]))
+                    input_list[index][level][1].setText(str(membership_range[index][level][3]))
+                elif level == "very_high":
+                    input_list[index][level][0].setText(str(membership_range[index][level][0]))
+                    input_list[index][level][1].setText(str(membership_range[index][level][1]))
+                else:
+                    input_list[index][level][0].setText(str(membership_range[index][level][0]))
+                    input_list[index][level][1].setText(str(membership_range[index][level][2]))
+        
+    def get_value_input(self):
+        membership_range = [self.velocity_membership, self.distance_membership, self.brake_membership]
+        input_list = [self.velocity_input, self.distance_input, self.brake_input]
+
+        for index in range(3):
+            try:
+                membership_range[index]["very_low"][0] = float(self.input_min_lis[index].text())
+                membership_range[index]["very_low"][1] = float(self.input_min_lis[index].text())
+            except:
+                membership_range[index]["very_low"][0] = 0
+                membership_range[index]["very_low"][1] = 0
+
+
+            try:
+                membership_range[index]["very_high"][2] = float(self.input_max_lis[index].text())
+                membership_range[index]["very_high"][3] = float(self.input_max_lis[index].text())
+            except:
+                membership_range[index]["very_high"][2] = 100
+                membership_range[index]["very_high"][3] = 100
+
+            for level in membership_range[index]:
+                if level == "very_low":
+                    try:
+                        membership_range[index][level][2] = float(input_list[index][level][0].text())
+                        membership_range[index][level][3] = float(input_list[index][level][1].text())
+                    except:
+                        membership_range[index][level][2] = 0
+                        membership_range[index][level][3] = 0
+                elif level == "very_high":
+                    try:
+                        membership_range[index][level][0] = float(input_list[index][level][0].text())
+                        membership_range[index][level][1] = float(input_list[index][level][1].text())
+                    except:
+                        membership_range[index][level][0] = 100
+                        membership_range[index][level][1] = 100
+                else:
+                    try:
+                        membership_range[index][level][0] = float(input_list[index][level][0].text())
+                        membership_range[index][level][1] = (float(input_list[index][level][0].text()) + float(input_list[index][level][1].text())) /2
+                        membership_range[index][level][2] = float(input_list[index][level][1].text())
+                    except:
+                        membership_range[index][level][0] = 0
+                        membership_range[index][level][1] = 0
+                        membership_range[index][level][2] = 0
+
     def deploy(self):
+
+        self.get_value_input()
+        self.fill_label()
+
         try:
             v_11 = float(self.ui.line_vel_verlow_min.text())
         except:
@@ -253,35 +418,51 @@ class Fuzzy(QMainWindow):
         
 
     def calculate(self):
-        # New Antecedent/Consequent objects hold universe variables and membership
-        # functions
-        velocity = ctrl.Antecedent(np.arange(0, self.vel_max, 0.5), 'velocity')
-        distance = ctrl.Antecedent(np.arange(0, self.dis_max, 0.5), 'distance')
-        brake = ctrl.Consequent(np.arange(0, self.brake_max, 0.5), 'brake')
+        # # New Antecedent/Consequent objects hold universe variables and membership
+        # # functions
 
-        # Custom membership functions can be built interactively with a familiar,
+        membership_range = [self.velocity_membership, self.distance_membership, self.brake_membership]
 
-        velocity['very_low'] = fuzz.trapmf(velocity.universe, self.vel_very_low)
-        velocity['low'] = fuzz.trimf(velocity.universe, self.vel_low)
-        velocity['medium'] = fuzz.trimf(velocity.universe, self.vel_med)
-        velocity['high'] = fuzz.trimf(velocity.universe, self.vel_high)
-        velocity['very_high'] = fuzz.trapmf(velocity.universe, self.vel_very_high)
+        velocity = ctrl.Antecedent(np.arange(membership_range[0]["very_low"][0], membership_range[0]["very_high"][3], 0.5), 'velocity')
+        distance = ctrl.Antecedent(np.arange(membership_range[1]["very_low"][0], membership_range[1]["very_high"][3], 0.5), 'distance')
+        brake = ctrl.Consequent(np.arange(membership_range[2]["very_low"][0], membership_range[2]["very_high"][3], 0.5), 'brake')
 
-        distance['very_low'] = fuzz.trapmf(distance.universe, self.dis_very_low)
-        distance['low'] = fuzz.trimf(distance.universe, self.dis_low)
-        distance['medium'] = fuzz.trimf(distance.universe, self.dis_med)
-        distance['high'] = fuzz.trimf(distance.universe, self.dis_high)
-        distance['very_high'] = fuzz.trapmf(distance.universe, self.dis_very_high)
+        # # Custom membership functions can be built interactively with a familiar,
 
-        brake['very_low'] = fuzz.trapmf(brake.universe, self.brake_very_low)
-        brake['low'] = fuzz.trimf(brake.universe, self.brake_low)
-        brake['medium'] = fuzz.trimf(brake.universe, self.brake_med)
-        brake['high'] = fuzz.trimf(brake.universe, self.brake_high)
-        brake['very_high'] = fuzz.trapmf(brake.universe, self.brake_very_high)
+        velocity['very_low'] = fuzz.trapmf(velocity.universe, membership_range[0]["very_low"])
+        velocity['low'] = fuzz.trimf(velocity.universe,membership_range[0]["low"])
+        velocity['medium'] = fuzz.trimf(velocity.universe, membership_range[0]["medium"])
+        velocity['high'] = fuzz.trimf(velocity.universe, membership_range[0]["high"])
+        velocity['very_high'] = fuzz.trapmf(velocity.universe, membership_range[0]["very_high"])
 
+        distance['very_low'] = fuzz.trapmf(distance.universe, membership_range[1]["very_low"])
+        distance['low'] = fuzz.trimf(distance.universe, membership_range[1]["low"])
+        distance['medium'] = fuzz.trimf(distance.universe, membership_range[1]["medium"])
+        distance['high'] = fuzz.trimf(distance.universe, membership_range[1]["high"])
+        distance['very_high'] = fuzz.trapmf(distance.universe, membership_range[1]["very_high"])
 
+        brake['very_low'] = fuzz.trapmf(brake.universe, membership_range[2]["very_low"])
+        brake['low'] = fuzz.trimf(brake.universe, membership_range[2]["low"])
+        brake['medium'] = fuzz.trimf(brake.universe, membership_range[2]["medium"])
+        brake['high'] = fuzz.trimf(brake.universe, membership_range[2]["high"])
+        brake['very_high'] = fuzz.trapmf(brake.universe, membership_range[2]["very_high"])
 
+        # membership_variabel = []
+        # membership_label = ["velocity", "distance", "brake"]
+        # membership_range = [self.velocity_membership, self.distance_membership, self.brake_membership]
 
+        # for index in range(3):
+        #     membership_variabel.append(ctrl.Antecedent(np.arange(membership_range[index]["very_low"][0], membership_range[index]["very_high"][3], 0.5), membership_label[index]))
+
+        #     membership_variabel[-1]['very_low'] = fuzz.trapmf(membership_variabel[-1].universe, membership_range[index]["very_low"])
+        #     membership_variabel[-1]['low'] = fuzz.trimf(membership_variabel[-1].universe, membership_range[index]["low"])
+        #     membership_variabel[-1]['medium'] = fuzz.trimf(membership_variabel[-1].universe, membership_range[index]["medium"])
+        #     membership_variabel[-1]['high'] = fuzz.trimf(membership_variabel[-1].universe, membership_range[index]["high"])
+        #     membership_variabel[-1]['very_high'] = fuzz.trapmf(membership_variabel[-1].universe, membership_range[index]["very_high"])
+
+        # velocity = membership_variabel[0]
+        # distance = membership_variabel[1]
+        # brake = membership_variabel[2]
 
         """
         =============================
@@ -362,6 +543,9 @@ class Fuzzy(QMainWindow):
         for i in reversed(range(self.ui.ver_lay1_2.count())): 
             self.ui.ver_lay1_2.itemAt(i).widget().setParent(None)
 
+        for i in reversed(range(self.ui.verticalLayout_8.count())): 
+            self.ui.verticalLayout_8.itemAt(i).widget().setParent(None)
+
         plt.rcParams['figure.constrained_layout.use'] = True
         a1,b1 = FuzzyVariableVisualizer(velocity).view()
         a2,b2 = FuzzyVariableVisualizer(distance).view()
@@ -402,7 +586,7 @@ class Fuzzy(QMainWindow):
         self.toolbar4 = NavigationToolbar(self.ui.canvas4, self)
         
         self.ui.ver_lay1_2.addWidget(self.ui.canvas4)
-        self.ui.ver_lay1_2.addWidget(self.toolbar4)
+        self.ui.verticalLayout_8.addWidget(self.toolbar4)
         
         
         brake = None
